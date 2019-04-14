905f575a4cd5ac4c5b6b2dc947e9e398b6143d80
Toastation <toastations@gmail.com>
2019-04-14 22:51:01 +0200

    chaos

---
+++ chaosequations/sketch.js
@@ -0,0 +1,18 @@
+
+function f(x, y) {
+  
+}
+
+function setup() {
+  width = windowWidth / 2;
+  height = windowHeight / 2;
+  let canvas = createCanvas(width, height);
+  canvas.parent("sketch");
+  c1 = color(255, 65, 65)
+  c2 = color(65, 65, 255);
+}
+
+function draw() {
+  background(0);
+
+}
