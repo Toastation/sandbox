51c96090cd205dbd6c6e417738cbbafb3516b764
Toastation <toastations@gmail.com>
2019-10-20 16:41:24 +0200

    anim mode

--- symmetry/sketch.js
+++ symmetry/sketch.js
@@ -1,6 +1,11 @@
-var nbLines = 5, nbLinesMax = 50, nbLinesMin = 1;
+let DRAWING_MODE = 0, ANIMATION_MODE = 1;
+let NB_LINE_MAX = 50, NB_LINES_MIN = 1;
+let nbLines = 5;
+let mode = DRAWING_MODE;
 let originX = undefined;
 let originY = undefined;
+let animX, animY, panimX, panimY;
+let a = 0;
 
 function setup() {
     width = windowWidth / 1.25;
@@ -11,36 +16,17 @@ function setup() {
     document.addEventListener('contextmenu', function(e) { if (e.button == 2) e.preventDefault(); });
     let div = createDiv();
     label = createP("Number of lines : "+nbLines);
-    slider = createSlider(1, 50, 5);
+    slider = createSlider(NB_LINES_MIN, NB_LINE_MAX, nbLines);
     slider.input(function () {nbLines = slider.value(); label.html("Number of lines : "+nbLines);});
     div.child(label);
     div.child(slider);
 }
 
 function draw() {
-    noStroke();
-    stroke(255, 0, 0);
-    if (mouseIsPressed && originX != undefined && originY != undefined) {
-        let deltaAngle = TAU / nbLines;
-        let angleMouse = atan2(mouseY - originY, mouseX - originX);
-        let anglePMouse = atan2(pmouseY - originY, pmouseX - originX);
-        let mouseDist = dist(originX, originY, mouseX, mouseY);
-        let pmouseDist = dist(originX, originY, pmouseX, pmouseY);
-        let mouseDif = HALF_PI - angleMouse;
-        let pmouseDif = HALF_PI - anglePMouse;
-        for (let i = 0; i < nbLines; i++) {
-            let axisAngle = i * deltaAngle + HALF_PI;
-            let mx = mouseDist * cos(axisAngle + mouseDif);
-            let my = mouseDist * sin(axisAngle +  mouseDif);
-            let pmx = pmouseDist * cos(axisAngle + pmouseDif);
-            let pmy = pmouseDist * sin(axisAngle + pmouseDif);
-            line(originX + pmx, originY + pmy, originX + mx, originY + my);
-            mx = mouseDist * cos(axisAngle - mouseDif);
-            my = mouseDist * sin(axisAngle - mouseDif);
-            pmx = pmouseDist * cos(axisAngle - pmouseDif);
-            pmy = pmouseDist * sin(axisAngle - pmouseDif);
-            line(originX + pmx, originY + pmy, originX + mx, originY + my);
-        }
+    if (mode == DRAWING_MODE && mouseIsPressed && originX != undefined && originY != undefined) {
+        drawLineWithSymmetry(originX, originY, mouseX, mouseY, pmouseX, pmouseY);
+    } else if (mode == ANIMATION_MODE) {
+        animate();
     }
     stroke(255, 255, 255);
     strokeWeight(1);
@@ -48,15 +34,77 @@ function draw() {
     rect(0, 0, width-1, height-1);
 }
 
+function animate() {
+    if (originX == undefined || originY == undefined) {
+        panimX = animX = originX = width / 2;
+        panimY = animY = originY = height / 2;
+    }
+    panimX = animX;
+    panimY = animY;
+    let deltaAngle = cos(millis() * 0.01);
+    let disp = randomGaussian(2, 0.5);
+    animX = animX + disp * cos(deltaAngle);
+    animY = animY + disp * sin(deltaAngle);
+    drawLineWithSymmetry(originX, originY, animX, animY, panimX, panimY);
+    if (animX < 0 || animX >= width || animY < 0 || animY >= height) {
+        originX = undefined;
+        originY = undefined;
+        background(0, 0, 0);
+    }
+}
+
+function drawLineWithSymmetry(ox, oy, px, py, ppx, ppy) {
+    strokeWeight(2);
+    stroke(255, 0, 0);
+    let deltaAngle = TAU / nbLines;
+    let angleMouse = atan2(py - oy, px - ox);
+    let anglePMouse = atan2(ppy - oy, ppx - ox);
+    let mouseDist = dist(ox, oy, px, py);
+    let pmouseDist = dist(ox, oy, ppx, ppy);
+    let mouseDif = HALF_PI - angleMouse;
+    let pmouseDif = HALF_PI - anglePMouse;
+    for (let i = 0; i < nbLines; i++) {
+        let axisAngle = i * deltaAngle + HALF_PI;
+        let mx = mouseDist * cos(axisAngle + mouseDif);
+        let my = mouseDist * sin(axisAngle +  mouseDif);
+        let pmx = pmouseDist * cos(axisAngle + pmouseDif);
+        let pmy = pmouseDist * sin(axisAngle + pmouseDif);
+        line(ox + pmx, oy + pmy, ox + mx, oy + my);
+        mx = mouseDist * cos(axisAngle - mouseDif);
+        my = mouseDist * sin(axisAngle - mouseDif);
+        pmx = pmouseDist * cos(axisAngle - pmouseDif);
+        pmy = pmouseDist * sin(axisAngle - pmouseDif);
+        line(ox + pmx, oy + pmy, ox + mx, oy + my);
+    }
+}
+
+/********* INPUTS *********/
+
 function mousePressed() {
-    if (mouseX >= 0 && mouseX < width && mouseY >= 0 && mouseY < height && mouseButton == LEFT) {
+    if (mode == DRAWING_MODE && mouseX >= 0 && mouseX < width && mouseY >= 0 && mouseY < height && mouseButton == LEFT) {
         originX = mouseX;
         originY = mouseY;
     } 
 }
 
 function mouseReleased() {
-    originX = undefined;
-    originY = undefined;
-    originalAngle = undefined;
+    if (mode == DRAWING_MODE) {
+        originX = undefined;
+        originY = undefined;
+        originalAngle = undefined;
+    }
+}
+
+function keyPressed() {
+    if (key == 'r') {
+        background(0, 0, 0);
+    }
+    if (key == 'm') {
+        originX = undefined;
+        originY = undefined;
+        originalAngle = undefined;
+        background(0, 0, 0);        
+        if (mode == DRAWING_MODE) mode = ANIMATION_MODE;
+        else mode = DRAWING_MODE;
+    }
 }
