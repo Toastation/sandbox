123f10b398c680c7d061911991ce87bc704b39f8
Toastation <toastations@gmail.com>
2019-10-20 13:29:42 +0200

    settings

--- symmetry/sketch.js
+++ symmetry/sketch.js
@@ -1,23 +1,26 @@
-let nbLines = 5;
-// let radius = 5;
+var nbLines = 5, nbLinesMax = 50, nbLinesMin = 1;
 let originX = undefined;
 let originY = undefined;
 
 function setup() {
     width = windowWidth / 1.25;
     height = windowHeight / 1.25;
+    colors = [color(255, 0, 0), color(0, 255, 0), color(0, 0, 255), color(255, 0, 255), color(255, 255, 0)];
     let canvas = createCanvas(width, height);
     canvas.parent("sketch");
+    document.addEventListener('contextmenu', function(e) { if (e.button == 2) e.preventDefault(); });
+    let div = createDiv();
+    label = createP("Number of lines : "+nbLines);
+    slider = createSlider(1, 50, 5);
+    slider.input(function () {nbLines = slider.value(); label.html("Number of lines : "+nbLines);});
+    div.child(label);
+    div.child(slider);
 }
 
 function draw() {
-    // background(255);
     noStroke();
     stroke(255, 0, 0);
-    color(255, 0, 0);
-    fill(255, 0, 0);
-    if (mouseIsPressed) {
-        push();
+    if (mouseIsPressed && originX != undefined && originY != undefined) {
         let deltaAngle = TAU / nbLines;
         let angleMouse = atan2(mouseY - originY, mouseX - originX);
         let anglePMouse = atan2(pmouseY - originY, pmouseX - originX);
@@ -32,21 +35,24 @@ function draw() {
             let pmx = pmouseDist * cos(axisAngle + pmouseDif);
             let pmy = pmouseDist * sin(axisAngle + pmouseDif);
             line(originX + pmx, originY + pmy, originX + mx, originY + my);
-            // circle(originX + mx, originY + my, radius);
             mx = mouseDist * cos(axisAngle - mouseDif);
             my = mouseDist * sin(axisAngle - mouseDif);
             pmx = pmouseDist * cos(axisAngle - pmouseDif);
             pmy = pmouseDist * sin(axisAngle - pmouseDif);
             line(originX + pmx, originY + pmy, originX + mx, originY + my);
-            // circle(originX + mx, originY + my, radius);
         }
-        pop();
     }
+    stroke(255, 255, 255);
+    strokeWeight(1);
+    noFill();
+    rect(0, 0, width-1, height-1);
 }
 
 function mousePressed() {
-    originX = mouseX;
-    originY = mouseY;
+    if (mouseX >= 0 && mouseX < width && mouseY >= 0 && mouseY < height && mouseButton == LEFT) {
+        originX = mouseX;
+        originY = mouseY;
+    } 
 }
 
 function mouseReleased() {
